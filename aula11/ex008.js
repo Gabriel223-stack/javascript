d17e5ff29eb325c228b02e6a98f881eb6009f63e
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-02-27 21:51:01 -0300

    condições if e else

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel} km`)
+
+//CONDIÇÃO SIMPLES
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. Multado!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')

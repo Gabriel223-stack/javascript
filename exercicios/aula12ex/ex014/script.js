577d91089ba1f9fcafd56c71bd8e755568dee5c1
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-03-06 21:13:57 -0300

    exercicio manha, noite e dia

---
+++ exercicios/aula12ex/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 18
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!!!
+        img.src = 'manha.png'
+        window.document.body.style.background = '#E7D1A7'
+    } else if (hora >= 12 && hora < 18) {
+        // Boa tarde!!
+        img.src = 'tarde.png'
+        window.document.body.style.background = '#D98673'
+    } else {
+        // Boa Noite!!
+        img.src = 'noite.png'
+        window.document.body.style.background = '#020659'
+    }
+
+}

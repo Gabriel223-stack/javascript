fb53388bc6c0ee6d601411c8807fcf4f783a545d
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-03-06 21:18:49 -0300

    manha, noite e dia

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -3,9 +3,9 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 18
+    var hora = 13
     msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 0 && hora < 12) {
+    if (hora >= 0 && hora <= 12) {
         // BOM DIA!!!
         img.src = 'manha.png'
         window.document.body.style.background = '#E7D1A7'

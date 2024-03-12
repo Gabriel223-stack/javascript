2b7bc2350215f88afdc384304dbf1f6577319c75
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-03-12 20:52:44 -0300

    repetições - while (loop)

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) {
+
+}
+
+
+/* while (c <= 100) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+/*console.log('Oi tudo bem?')
+console.log('Oi tudo bem?')
+console.log('Oi tudo bem?')
+console.log('Oi tudo bem?')
+console.log('Oi tudo bem?')
+console.log('Oi tudo bem?')
+*/

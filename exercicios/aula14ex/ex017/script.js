59a9611093d0c95900d871afdcf0d419cbad95da
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-03-29 16:05:25 -0300

    tabela de multiplicacao, for, while

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,31 @@
-alert('Olá!')
+function tabuada() {
+    /* ADICIONAR VALOR NOS ELEMENTOS */
+    let num = document.getElementById('txtn') //NUMERO ENTRADA
+    let tab = document.getElementById('seltab') //SAIDA DO RESULTADO
+    
+    if (num.value.length == 0)
+    /* SE O VALOR FOR IGUAL A ZERO AVISAR */
+    {
+        window.alert('por favor, adicione um número!')
+    } else
+    /* SE O VALOR NÃO FOR... */
+    {
+        let n = Number(num.value)
+        let c = 1
+        
+
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            tab.appendChild(item)
+            item.text = (`${n} x ${c} = ${n * c}`)
+        }
+
+        /* TAMBÉM FUNCIONA COM 
+        WHILE (C <= 10) {
+        LET ITEM = DOCUMENT.CREATEELEMENT('')
+        TAB.APPENDCHILD(ELEMENTOFILHO)
+        ITEM.TEXT = RESULTADO A AMOSTRA
+        }
+        */
+    }
+}

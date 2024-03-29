ed02a0cec7562ca75acf13aa1f30da1822a8eda8
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-03-29 18:51:35 -0300

    desafio concluido temperatura

---
+++ desafios/d006/script.js
@@ -0,0 +1,11 @@
+function conversor() {
+    let val = Number(window.prompt('Digite uma temperatura e, °C (Celsius'))
+    let res = document.getElementById('res')
+
+    res.innerHTML = '' //ADICIONAR ISTO, LIMITA O CLIQUE PARA UMA VEZ DE CADA, E NÃO FICA ACUMULANDO RESPOSTAS
+
+    res.innerHTML += `<h1>A temperatura de ${val.toLocaleString('pt-BR')}°C, corresponde a...<h1/>`
+    res.innerHTML += `<p>${(val * 9 / 5 + 32).toLocaleString('pt-BR')}° F (FAHRENHEIT)<p/>`
+    res.innerHTML += `<p>${(val + 273.15).toLocaleString('pt-BR')}° K (KELVIN)`
+
+}

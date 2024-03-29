f22a64077569be0905d4da806f88115920c08fce
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-03-29 17:14:40 -0300

    usando for em contagem

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,37 +1,34 @@
 function contar() {
-    // PUXANDO OS ELEMENTOS
+    /* ADICIONANDO VALORES NOS ELEMENTOS */
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
+    let pass = document.getElementById('txtp')
     let res = document.getElementById('res')
-    // FIM DOS ELEMENTOS
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.lenght == 0) {
-        //window.alert('[ERRO] Falta dados!')
+    /* VALOR DAS VARIAVEIS PARA AVISAR SE NÃO PREENCHER */
+    if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
+        //window.alert('Por favor, adicione os números para que possa funcionar')
         res.innerHTML = 'Impossível contar!'
-    } else {
-        //CONVERTENDO A STRING PARA NUMBER
-        res.innerHTML = `Contando: <br>`
+    }
+    /* CONVERSÃO DE STRING PARA NUMBER */
+    else {
+        res.innerHTML = `Contando: <br/>`
         let i = Number(ini.value)
         let f = Number(fim.value)
-        let p = Number(passo.value)
+        let p = Number(pass.value)
 
-        if (p <= 0) {
+        if (p <= 0) /* DEFININDO AVISO DE ERRO */ {
             window.alert('Passo Inválido! Considerando PASSO 1')
         }
-        // USANDO FOR
-        if (i < f) {
-            // CONTAGEM CRESCENTE
+
+        if (i < f) /* INICIO FOR MENOR QUE FIM */ {
             for (let c = i; c <= f; c += p) {
-                res.innerHTML += `${c} \u{1F449}`
-            } 
-        } else {
-            // CONTAGEM REGRESSIVA  
-            for (let c = i; c >= f; c -= p) {
-                res.innerHTML += ` ${c} \u{1F449}`
+                res.innerHTML += `\u{1f449} ${c}`
             }
-            // FIM FOR
-        }
-        res.innerHTML += `\u{1F3C1}`
+        } else /* INICIO FOR MAIOR QUE FIM */{
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `\u{1f449}${c}`
+            }
+        } res.innerHTML += `\u{1f3c6} Congratulations!`
     }
 }
